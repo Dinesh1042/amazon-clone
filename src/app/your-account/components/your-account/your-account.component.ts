d84024fd18c031c3c5593e1186ae5211c8ab9672
Dinesh1042 <dineshram1042002@gmail.com>
2022-01-02 14:35:58 +0530

    add admin options in your account

--- src/app/your-account/components/your-account/your-account.component.ts
+++ src/app/your-account/components/your-account/your-account.component.ts
@@ -1,18 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { User } from 'shared/models/user';
 import { AuthService } from 'shared/services/auth.service';
+import { UserService } from 'shared/services/user.service';
 
 @Component({
   selector: 'your-account',
   templateUrl: './your-account.component.html',
   styleUrls: ['./your-account.component.scss'],
 })
-export class YourAccountComponent {
+export class YourAccountComponent implements OnInit, OnDestroy {
+  user?: User;
+  pageLoading = false;
+  error: Error | null = null;
+
+  private subscription = new Subscription();
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
+    private userService: UserService,
     private authService: AuthService
   ) {}
+
+  ngOnInit(): void {
+    this.pageLoading = true;
+
+    this.subscription.add(
+      this.userService.appUser$.subscribe(
+        this.handleUserSuccess.bind(this),
+        this.handleUserSuccess.bind(this)
+      )
+    );
+  }
+
+  private handleUserSuccess(user: User) {
+    this.user = user;
+    this.pageLoading = false;
+  }
+
+  private handleUserError(error: Error) {
+    this.error = error;
+    this.pageLoading = false;
+  }
 
   navigate(path: string) {
     this.router.navigate([path], { relativeTo: this.route });
@@ -20,5 +51,9 @@ export class YourAccountComponent {
 
   logout() {
     this.authService.signOut();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
